2930864d9e0f864cbf74f696b0c444938819cede
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-21 23:26:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_second_tier
 	    :names => {}
 	    :rent_in_dollars => {}
 	  }
-	 	}
+	}
 end
 
 def monopoly_with_third_tier
@@ -30,13 +30,15 @@ def monopoly_with_third_tier
   monopoly = {
 	  :railroads => {
 	    :pieces => 4
-	    :names => {
+	    :names => 
+	    {
 	      :reading_railroad => {}
 	      :pennsylvania_railroad => {}
 	      :b_and_o_railroad => {}
 	      :shortline => {}
 	    }
-	    :rent_in_dollars => {
+	    :rent_in_dollars => 
+	    {
 	      :one_piece_owned => 25
 	      :two_pieces_owned => 50
 	      :three_pieces_owned => 100
